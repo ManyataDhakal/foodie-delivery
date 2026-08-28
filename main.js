8bce6488d42393fbfe37e757d7d1ce506ad70b1c
Manyata Dhakal <manyatadhakal12@gmail.com>
2026-08-28 17:59:43 +0545

    Fetch product data and create product cards

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
- var swiper = new Swiper('.mySwiper', {
+var swiper = new Swiper(".mySwiper", {
   loop: true,
-        navigation: {
-          nextEl: '#next',
-          prevEl: '#prev',
-        },
-      });
+  navigation: {
+    nextEl: "#next",
+    prevEl: "#prev",
+  },
+});
 
-      const cartIcon = document.querySelector('.cart-icon');
-      const cartTab = document.querySelector('.cart-tab');
-      const closeBtn = document.querySelector('.close-btn');
+const cartIcon = document.querySelector(".cart-icon");
+const cartTab = document.querySelector(".cart-tab");
+const closeBtn = document.querySelector(".close-btn");
 
-      cartIcon.addEventListener('click', ()=> cartTab.classList.add('cart-tab-active'));
-      closeBtn.addEventListener('click', ()=> cartTab.classList.remove('cart-tab-active'));
+cartIcon.addEventListener("click", () =>
+  cartTab.classList.add("cart-tab-active"),
+);
+closeBtn.addEventListener("click", () =>
+  cartTab.classList.remove("cart-tab-active"),
+);
+
+let productList = [];
+
+const showCards = () => {
+  productList.forEach((product) => {
+    const orderCard = document.createElement("div");
+    orderCard.classList.add("order-card");
+
+    orderCard.innerHTML = `
+          <div class="card-image">
+                <img src="./images/burger.png">
+              </div>
+              <h4>Cheese Burger</h4>
+              <h4 class="price">$200</h4>
+              <a href="#" class="btn">Add to Cart</a>
+           `;
+  });
+};
+
+const initApp = () => {
+  fetch("products.json")
+    .then((response) => response.json())
+    .then((data) => {
+      productList = data;
+      showCards();
+    });
+};
+
+initApp();
