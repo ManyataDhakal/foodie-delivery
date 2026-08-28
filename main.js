cea52d50f0a88aadff5406bf8f3fa567be6f2485
Manyata Dhakal <manyatadhakal12@gmail.com>
2026-08-28 17:21:56 +0545

    Add the close button in the main.js

--- main.js
+++ main.js
@@ -8,5 +8,7 @@
 
       const cartIcon = document.querySelector('.cart-icon');
       const cartTab = document.querySelector('.cart-tab');
+      const closeBtn = document.querySelector('.close-btn');
 
       cartIcon.addEventListener('click', ()=> cartTab.classList.add('cart-tab-active'));
+      closeBtn.addEventListener('click', ()=> cartTab.classList.remove('cart-tab-active'));
